ef4484a113d1a833e1974717814d6615e5f332fd
yanghongwei <qq342908400@gmail.com>
2020-09-12 14:37:02 +0800

    feat: 使用自定义function代替固定的snakbar

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,17 +1,30 @@
+import 'package:bot_toast/bot_toast.dart';
 import 'package:double_back_to_close_app/double_back_to_close_app.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(Example());
 
-class Example extends StatelessWidget {
+class Example extends StatefulWidget {
+  Example({Key key}) : super(key: key);
+
+  @override
+  _ExampleState createState() => _ExampleState();
+}
+
+class _ExampleState extends State<Example> {
+  Future onBackClick() async {
+    print('back~·');
+    BotToast.showText(text: 'Tap back again to leave');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: BotToastInit(), //1. call BotToastInit
+      navigatorObservers: [BotToastNavigatorObserver()],
       home: Scaffold(
         body: DoubleBackToCloseApp(
-          snackBar: const SnackBar(
-            content: Text('Tap back again to leave'),
-          ),
+          onBackShow: onBackClick,
           child: Center(
             child: OutlineButton(
               child: const Text('Tap to simulate back'),
@@ -24,3 +37,31 @@ class Example extends StatelessWidget {
     );
   }
 }
+
+// class Example extends StatelessWidget {
+
+//   dynamic onBackClick() {
+//     showDialog(context: context);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: DoubleBackToCloseApp(
+//           snackBar: const SnackBar(
+//             content: Text('Tap back again to leave'),
+//           ),
+//           onBackShow: onBackClick,
+//           child: Center(
+//             child: OutlineButton(
+//               child: const Text('Tap to simulate back'),
+//               // ignore: invalid_use_of_protected_member
+//               onPressed: WidgetsBinding.instance.handlePopRoute,
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
